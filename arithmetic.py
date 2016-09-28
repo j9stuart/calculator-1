4b58c889c9880d170b0f82d99d259c85a95f61ad
Hackbright Student <no-reply@hackbrightacademy.com>
2016-09-28 12:27:25 -0700

    Put in docstrings and completed return functions

--- arithmetic.py
+++ arithmetic.py
@@ -1,23 +1,31 @@
 def add(num1, num2):
-    return num1 + num2
+	"""Returns the sum of the inputs"""
+	return num1 + num2
 
 def subtract(num1, num2):
+    """Returns the result of subtracting the second number from the first"""
     return num1 - num2
 
 def multiply(num1, num2):
-    pass
+	"""Returns the product of the inputs"""
+	return num1 * num2
 
 def divide(num1, num2):
-    pass
+	"""Divides the first input by the second, returning a floating point number"""
+	return num1 / num2
 
 def square(num1):
-    pass
+	"""Returns the square of the input"""
+	return num1 ** 2
 
 def cube(num1):
-    pass
+	"""Returns the cube of the input"""
+	return num1 ** 3
 
 def power(num1, num2):
-    pass
+	"""Returns the result of raising the first input to the power of the second input"""
+	return num1**num2
 
 def mod(num1, num2):
-    pass
+	"""Returns the remainder when the first input is divided by the second input"""
+	return num1 % num2

c8cee7e9feef764d7ab5023bbca7b4fc8104a4e0
Hackbright Student <no-reply@hackbrightacademy.com>
2016-09-28 12:48:31 -0700

    Made style changes to improve and simplify code

--- arithmetic.py
+++ arithmetic.py
@@ -12,7 +12,7 @@ def multiply(num1, num2):
 
 def divide(num1, num2):
 	"""Divides the first input by the second, returning a floating point number"""
-	return float(num1) / float(num2)
+	return float(num1) / num2
 
 def square(num1):
 	"""Returns the square of the input"""
@@ -24,7 +24,7 @@ def cube(num1):
 
 def power(num1, num2):
 	"""Returns the result of raising the first input to the power of the second input"""
-	return num1**num2
+	return num1 ** num2
 
 def mod(num1, num2):
 	"""Returns the remainder when the first input is divided by the second input"""

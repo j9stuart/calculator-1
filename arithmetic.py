edf4f39adcd857f5b5d3a082ef320960d62e2e77
Hackbright Student <no-reply@hackbrightacademy.com>
2016-09-28 12:32:49 -0700

    Added float to the division function

--- arithmetic.py
+++ arithmetic.py
@@ -12,7 +12,7 @@ def multiply(num1, num2):
 
 def divide(num1, num2):
 	"""Divides the first input by the second, returning a floating point number"""
-	return num1 / num2
+	return float(num1) / float(num2)
 
 def square(num1):
 	"""Returns the square of the input"""
